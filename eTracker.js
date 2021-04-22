0a4559562455f79f971a7d474eedb52617d93b64
gigi <guka.tgs@gmail.com>
2021-04-22 15:35:20 -0400

    fixed the bugg

--- eTracker.js
+++ eTracker.js
@@ -1,35 +1,37 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const pass = require("./config");
+
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
 
   // Your port; if not 3306
   port: 3306,
 
   // Your username
-  user: "root",
+  user: 'root',
 
   // Be sure to update with your own MySQL password!
   password: pass,
-  database: "employee_DB";
+  database: 'employee_DB',
 });
 
-const start = () {
-  inquirer.prompt({
-    type: "list",
-    message: "What would you like to do?",
-    name: "option",
-    choices: [
-      "View all departments",
-      "View all Roles",
+// const start = () {
+//   inquirer.prompt({
+//     type: 'list',
+//     message: 'What would you like to do?',
+//     name: 'option',
+//     choices: [
+//       'View all departments',
+//       'View all Roles',
 
-    ]
-  })
-}
+//     ]
+//   })
+// }
 
 // Connect to the DB
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}\n`);
-  start();
+  // start();
 });
